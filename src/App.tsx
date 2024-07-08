3baa4bcb7a666980362f9b436e89754e497ddac2
Bhawani Shankar Bharti <bhawani.bharti@techolution.com>
2024-07-08 15:45:55 +0530

    feat: add sample lines

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div><p>With coding standards check</p></div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
